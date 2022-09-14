d2004b6973ee806351bad11d418bee8a7706d5e6
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-09-14 09:01:02 -0700

    Remove and add elements to the 4 variables

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,12 @@ students = ["Henry," "Sam", "Luke", "Cynthia"]
 ticket_prices = [25, 35, 90, 270]
 venmo_balances = [26.75, 457.32, 0.12, 75.01]
 booleans = [true, true, false, false]
+
+# this will remove the last element in the students variable
+students.pop
+# This will remove the first element in the ticket_prices variable
+ticket_prices.shift
+# This will add an element to the end of the venmo_balances variable
+venmo_balances.push (27.65)
+# This will add an element to the begining of the booleans array
+booleans.unshift (false)
